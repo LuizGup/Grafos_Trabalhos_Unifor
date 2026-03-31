887bb4403add417f15c5b299173eef5a498e387f
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2026-03-31 17:55:24 -0300

    Add __iter__ method to LinkIterator class for iterator protocol compliance

--- AV2/T2/src/utils/linklist.py
+++ AV2/T2/src/utils/linklist.py
@@ -10,6 +10,9 @@ class LinkIterator:
     def __init__(self, current):
         self.current = current
 
+    def __iter__(self):
+        return self
+
     def __next__(self):
         if self.current is None:
             raise StopIteration()
